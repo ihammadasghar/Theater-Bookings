0ecc2878e764165a4054cb49156f3b8a639172ea
Hammad Asghar <ihammadasghar@gmail.com>
2022-05-27 14:46:51 +0100

    Add notes about using flask_sqlalchemy in controllers

--- theatrebookings/controllers/UserController.py
+++ theatrebookings/controllers/UserController.py
@@ -1,6 +1,15 @@
 from models import Reservation, User
-from . import db
+from .. import db
 
+# db functions syntax:
+# db.session.add(item) create item
+# db.session.delete(item) delete item
+# db.session.commit() save changes NOTE: Use this after any changes to the db
+
+# User functions syntax:
+# Find user with id: User.query.get(id)
+# Get list of all users: User.query.all()
+# more examples of querying ways: https://flask-sqlalchemy.palletsprojects.com/en/2.x/queries/
 
 def create():
     # TODO: get information about user in parameters, make a user object and save it in the data base 
